626e928814a86cfc10c8f57de7b7d749c3d4e156
kauanny-cmd <kauannyvieira8@gmail.com>
2022-04-24 18:22:04 -0300

    criando os cactus e o intervalo de pulos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 const dino = document.querySelector('.dino')
+const background = document.querySelector('.background')
 let isJumping = false
-
+let position = 0
 
 function handleKeyUp(event) {
-    if (event.keyCode === 32) {
+    // add os 3 tipos de keyUp
+    if (event.keyCode === 32 || event.keyCode === 87 || event.keyCode === 38) {
         if(!isJumping){
             jump()
         }
@@ -11,8 +13,6 @@ function handleKeyUp(event) {
 }
 
 function jump() {
-    let position = 0
-
     isJumping = true
 
     let upInterval = setInterval(() => {
@@ -37,4 +37,30 @@ function jump() {
     }, 20)
 }
 
+function createCactus(){
+    const cactus = document.createElement('div')
+    let cactusPosition = 1000
+    let randomTime = Math.random() * 6000
+
+    cactus.classList.add('cactus')
+    cactus.style.left = 1000 + 'px'
+    background.appendChild(cactus)
+
+    let leftInterval = setInterval(() =>{
+        if(cactusPosition < -60){
+            clearInterval(leftInterval)
+            background.removeChild(cactus)
+        } else if(cactusPosition > 0 && cactusPosition < 60 && position < 60){
+            clearInterval(leftInterval)
+            document.body.innerHTML = "<h1 class='game-over'> Fim de jogo! </h1>"
+        } else{
+            cactusPosition -= 10
+            cactus.style.left = cactusPosition + 'px'
+        }
+    }, 15)
+
+    setTimeout(createCactus, randomTime)
+}
+
+createCactus()
 document.addEventListener('keyup', handleKeyUp)
