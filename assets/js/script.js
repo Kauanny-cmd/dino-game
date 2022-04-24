954a8b7b40eaf7ba8cd91528fa3c1f9020649a67
kauanny-cmd <kauannyvieira8@gmail.com>
2022-04-24 17:50:30 -0300

    subindo o dino

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+const dino = document.querySelector('.dino')
+
+function handleKeyUp(event){
+    if(event.keyCode === 32){
+        jump();
+    }
+}
+
+function jump(){
+    let position = 0
+
+    let upInterval = setInterval(() =>{
+
+        //sobe
+        position += 20
+        dino.style.bottom = position + 'px'
+    },20)
+}
+
+document.addEventListener('keyup', handleKeyUp)
